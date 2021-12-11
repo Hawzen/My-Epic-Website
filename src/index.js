a3da0f29ef67727bf6405fafced3b5612f3698a2
Hawzen <hawzen@users.noreply.github.com>
2021-12-12 00:04:52 +0300

    Add og link preview that doesnt work

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from "react-router-dom";
 
+import profileImg from "./assets/profile.jpg";
+
 ReactDOM.render(
     <React.StrictMode>
       <meta
         name="viewport"
         content="minimum-scale=1, initial-scale=1, width=device-width"
       />
+      <meta property="og:description" content="This website is a personal repository of projects." />
+      <meta property="og:image" content="https://avatars.githubusercontent.com/u/43524721?v=4"/>
+      <meta property="og:site_name" content="Hawzen" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Hawzen's repo" />
+      <meta property="og:url" content="https://www.hawzen.me" />
+
       <HashRouter>
       <App/>
       </HashRouter> 
